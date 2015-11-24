dc81b71f4f4f337d88b972d19580501df76bb4a0
Jabali <jabali@iMac-de-Jabali.local>
2015-11-24 16:50:53 +0100

    task3

--- app/src/main/java/ihm/ihm_p2/MainActivity.java
+++ app/src/main/java/ihm/ihm_p2/MainActivity.java
@@ -1,31 +1,59 @@
 package ihm.ihm_p2;
 
-import android.app.ListActivity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
 
 
-
-public class MainActivity extends ListActivity {
-
+public class MainActivity extends Activity implements OnFragmentInteractionListener{
+    boolean tablet = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Comprobar si esta cargando lal ayout de tablet o la de telefono
+        List_Fragment listapueblos = new List_Fragment();
+        if(findViewById(R.id.layout_info_tablet)!=null) {
+            tablet=true;
+            getFragmentManager().beginTransaction().add(R.id.layout_list_tablet, listapueblos).commit();
+            System.out.println("tablet");
+        }
+        else {
+            getFragmentManager().beginTransaction().add(R.id.frag_layout_phone, listapueblos).commit();
+            System.out.println("phone");
+        }
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        Intent intent = new Intent(this, Activity_infopueblo.class);
-        intent.putExtra("npueblo", (String) l.getItemAtPosition(position));
-        System.out.println(l.getItemAtPosition(position));
-        startActivity(intent);
+    public void onFragmentInteraction(String nombre) {
+        info_fragment infopueblo = new info_fragment();
+        Bundle args = new Bundle();
+        args.putString("name",nombre);
+        infopueblo.setArguments(args);
+
+        if(tablet){
+
+            info_fragment info_frag = (info_fragment)getFragmentManager().findFragmentById(R.id.layout_info_tablet);
+            if(info_frag==null || info_frag.get_name()!=nombre) {
+                info_frag = info_fragment.newInstance(nombre);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.layout_info_tablet, infopueblo);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        }
+        else{
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frag_layout_phone, infopueblo);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+
+        }
 
 
+        //cambio de layout∫
     }
 
 
-}
+
---
+++ app/src/main/java/ihm/ihm_p2/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package ihm.ihm_p2;
+
+/**
+ * Created by jabali on 4/11/15.
+ */
+public interface OnFragmentInteractionListener {
+    public void onFragmentInteraction(String nombrepueblo);
+}
